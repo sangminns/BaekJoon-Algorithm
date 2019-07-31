404ac5c5fa2e25609bcb8cdca5643ed7f8f1c720
ksm357 <ksm3725@naver.com>
2019-07-31 11:39:53 +0900

    counting sort

---
+++ Algorithm-Practice/sort-counting.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+    int temp;
+    int count[5];
+    int arr[30] = {
+        1, 3, 2, 4, 3, 2, 5, 3, 1, 2,
+        3, 4, 4, 3, 5, 1, 2, 3, 5, 2,
+        3, 1, 4, 3, 5, 1, 2, 1, 1, 1
+    };
+
+    for(int i=0; i<5; i++) {
+        count[i] = 0;
+    }
+
+    for(int i=0; i<30; i++) {
+        count[arr[i]-1]++;
+    }
+
+    for(int i=0; i<5; i++) {
+        if(count[i] != 0) {
+            for(int j=0; j<count[i]; j++) {
+                printf("%d ", i+1);
+            }
+        }
+    }
+
+    return 0;
+}
